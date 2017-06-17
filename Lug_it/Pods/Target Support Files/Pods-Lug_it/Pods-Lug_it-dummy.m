16195b095d4ba9aa29e4779f4ba5e7a3f0cfcfd3
yuso95 <younoussa.ousmane@icloud.com>
2017-06-17 14:07:02 -0400

    Implement Firebase in the app

---
+++ Lug_it/Pods/Target Support Files/Pods-Lug_it/Pods-Lug_it-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lug_it : NSObject
+@end
+@implementation PodsDummy_Pods_Lug_it
+@end
---
+++ Lug_it/Pods/Target Support Files/Pods-Lug_it/Pods-Lug_it-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Lug_itVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Lug_itVersionString[];
+
